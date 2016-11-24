8d77b9ea2a423e9c9bf739ab19c02a804c19b58f
sachin <sachinshinde7676@gmail.com>
2016-11-24 20:18:49 +0530

    Uploaded library on Pypy and updated documentation

--- setup.py
+++ setup.py
@@ -4,11 +4,17 @@ setup(
     name='socketclusterclient',
     packages=['socketclusterclient'],  # this must be the same as the name above
     version='0.1',
-    description='Test library for socketcluster client',
+    description='Client library for socketcluster framework in nodejs',
     author='Sachin Shinde',
     author_email='sachinshinde7676@gmail.com',
     url='https://github.com/sacOO7/socketcluster-client-python',  # use the URL to the github repo
     download_url='https://github.com/sacOO7/socketcluster-client-python/tarball/v0.1',  # I'll explain this in a second
-    keywords=['websocket', 'socketcluster', 'nodejs', 'client'],  # arbitrary keywords
+    keywords=['websocket', 'socketcluster', 'nodejs', 'client', 'socketclusterclient'],  # arbitrary keywords
     classifiers=[],
 )
+
+
+# python setup.py register -r pypitest
+# python setup.py sdist upload -r pypitest
+# python setup.py register -r pypi
+# python setup.py sdist upload -r pypi
