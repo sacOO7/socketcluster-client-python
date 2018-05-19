ceb065173ef3164146cd49c987b83181dac02141
sachin <sachinshinde7676@gmail.com>
2018-05-19 21:26:43 +0530

    Updated setup.py, released new version to pypi

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 from distutils.core import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
     name='socketclusterclient',
     packages=['socketclusterclient'],  # this must be the same as the name above
-    version='1.3.0',
+    version='1.3.1',
     description='Client library for socketcluster framework in nodejs',
+    long_description=readme(),
     author='Sachin Shinde',
     author_email='sachinshinde7676@gmail.com',
     license='MIT',
     url='https://github.com/sacOO7/socketcluster-client-python',  # use the URL to the github repo
-    download_url='https://github.com/sacOO7/socketcluster-client-python/tarball/v1.3.0',  # I'll explain this in a second
+    download_url='https://github.com/sacOO7/socketcluster-client-python/tarball/v1.3.1',
     keywords=['websocket', 'socketcluster', 'nodejs', 'client', 'socketclusterclient'],  # arbitrary keywords
     install_requires=[
-          'websocket-client',
-      ],
+        'websocket-client',
+    ],
     classifiers=[],
 )
 
