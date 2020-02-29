a1b9208883a74fc15f99f12ad3e29bbb3dd68fc2
sacOO7 <sachinshinde7676@gmail.com>
2020-02-29 14:27:16 +0530

    updated setup.py and gitignore file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 import os.path
-
+import setuptools
 
 def readme():
     if os.path.exists('README.md'):
@@ -10,14 +10,15 @@ def readme():
         return "Python client for socket-cluster framework in node.js"
 
 
-version = '1.3.5'
+version = '1.3.6'
 
 setup(
     name='socketclusterclient',
-    packages=['socketclusterclient'],  # this must be the same as the name above
+    packages=setuptools.find_packages(),  # this must be the same as the name above
     version=version,
     description='Client library for socketcluster framework in nodejs',
     long_description=readme(),
+    long_description_content_type="text/markdown",
     author='Sachin Shinde',
     author_email='sachinshinde7676@gmail.com',
     license='MIT',
@@ -27,14 +28,27 @@ setup(
     install_requires=[
         'websocket-client<=0.48',
     ],
-    classifiers=[],
+    classifiers=[
+        "Programming Language :: Python :: 2/3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
 )
 
 # python setup.py register -r pypitest
-# python setup.py sdist upload -r pypitest
 # python setup.py register -r pypi
 # python setup.py sdist upload -r pypi
-# python3 -m twine upload dist/
+# python3 -m twine upload dist/*
+
+## register
+
+# python setup.py register -r pypitest
+# python setup.py register -r pypi
+
+## Build and upload
+# python setup.py sdist bdist_wheel
+# python3 -m twine upload dist/*
 
 # git tag -a v1.3.4 -m "Fixed #16, websocket client version issue"
 # git push --tags origin
